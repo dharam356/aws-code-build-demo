d2e26482cfe4decf31cbf2f1637722e113ccf4c0
Dharmendra_Kumar <dharm356@gmail.com>
2023-04-12 17:10:53 +0530

    update script

--- scripts/server_validate.sh
+++ scripts/server_validate.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
+status=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost:8008)
 while [ true ]
 do
-    if [ "$(curl -s http://localhost:5000)" = '{"status":"OK"}' ]
+    if [ ${status} = '{"status":"OK"}' ]
     then
         exit 0
     else
         echo "check server is running?"
+        echo ${status}
         sleep 3s
     fi
 done
