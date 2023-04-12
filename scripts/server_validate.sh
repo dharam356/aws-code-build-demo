84e46886eab4bb16da2728d00a022506bbc4c6f1
Dharmendra_Kumar <dharm356@gmail.com>
2023-04-12 18:14:55 +0530

    update script test

--- scripts/server_validate.sh
+++ scripts/server_validate.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 status=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost:8008)
+echo ${status}
 while [ true ]
 do
-    if [ ${status} = '200' ]
+    if [ ${status} == '200' ]
     then
         exit 0
     else
