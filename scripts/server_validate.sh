bc5a7489db00c282daec1a9da744dd20ff821f92
dharam356 <114485543+dharam356@users.noreply.github.com>
2023-04-12 18:23:11 +0530

    Update server_validate.sh

--- scripts/server_validate.sh
+++ scripts/server_validate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-status=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost:8008)
+status=$(curl --write-out %{http_code} --silent --output /dev/null http://34.230.42.243:8008/)
 echo ${status}
 while [ true ]
 do
