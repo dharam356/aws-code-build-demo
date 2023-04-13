d19614c711f0393a3029f19102d8f58e223e884b
Dharmendra_Kumar <dharm356@gmail.com>
2023-04-13 15:06:24 +0530

    update script test 2

--- scripts/server_validate.sh
+++ scripts/server_validate.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-status=$(curl --write-out "\%{http_code}" --silent --output /dev/null http://localhost:8008)
-echo "\${status}"
+export status=$(curl --write-out "\%{http_code}" --silent --output /dev/null http://localhost:8008)
+echo ${status}
 while true
 do
-    if [ 200 == "\${status}"  ]
+    if [ 200 == ${status} ]
     then
         exit 0
     else
         echo "check server is running?"
-        echo "\${status}"
+        echo ${status}
         sleep 3s
     fi
 done
