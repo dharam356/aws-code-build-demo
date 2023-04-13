52992dc5c5232dad059130955bfd32ab2ecb7f28
Dharmendra_Kumar <dharm356@gmail.com>
2023-04-13 17:42:07 +0530

    update script test 6

--- scripts/server_executables_permission.sh
+++ scripts/server_executables_permission.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-chmod +x /home/ec2-user/server/target/aws-cicd-code-build.jar
-chmod +x /home/ec2-user/server/*.sh
+sudo chmod +x /home/ec2-user/server/target/aws-cicd-code-build.jar
+sudo chmod +x /home/ec2-user/server/*.sh
